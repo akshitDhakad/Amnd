c8ad1625e2a2580dbdc48d870b5e85680a1a9040
Akshit Dhakad <116799118+akshitDhakad@users.noreply.github.com>
2024-06-06 00:31:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Home from "./app/Home";
+"use strict";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/app/components/Layout.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar.jsx";
+import Footer from "./Footer.jsx";
+
+function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
