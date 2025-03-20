7ba93dc519cb295630786de3ffae253941d769e8
Akshit Dhakad <116799118+akshitDhakad@users.noreply.github.com>
2025-03-21 01:19:21 +0530

    Add ContactForm component and enhance Banner with icon and styling

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,25 +1,14 @@
 import React from "react";
 // component imports
-import { Link } from "react-router-dom";
+
 import Layout from "../components/Layout.jsx";
 import ServicesBanner from "../components/ServicesBanner.jsx";
-import Testinomial from "../components/Testinomial.jsx";
-import FlipCard from "../components/FlipCard.jsx";
-import Carousel from "../components/Carousel.jsx";
-import ClientCard from "../components/ClientCard.jsx";
 
-import HoverCard from "../components/HoverCard.jsx";
 // react icons
 import { HiOutlineArrowLongRight } from "react-icons/hi2";
-import {
-  FaFacebook,
-  FaLinkedin,
-  FaInstagram,
-  FaArrowUpRightFromSquare,
-} from "react-icons/fa6";
-import { IoMdKey } from "react-icons/io";
 import BlogTech from "../components/BlogTech.jsx";
 import BlogCard from "../components/BlogCard.jsx";
+import ContactFrom from "../components/ContactFrom.jsx";
 
 export default function Blog() {
   const backgroundImg = {
@@ -129,139 +118,8 @@ export default function Blog() {
 
         {/* Contact us Form */}
         <section>
-          <div className="relative max-w-6xl px-4 py-20 grid grid-cols-2 gap-2  m-auto">
-            <span class="absolute font-semibold text-[12rem] font-theme-font leading-none opacity-10 top-[-25px] left-[-125px] text-gray-500">
-              Let's Talk
-            </span>
-            {/* heading  */}
-            <div className="col-span-2 flex flex-col ">
-              <div className="mb-2">
-                <h2 className="text-xl mdtext-4xl text-gray-600 font-bold tracking-wide font-theme-font">
-                  Contact Us
-                </h2>
-              </div>
-              <div className="mb-4">
-                <h1 className="text-3xl md:text-5xl text-black font-bold tracking-wide font-theme-font">
-                  Let's Talk
-                </h1>
-              </div>
-            </div>
-
-            <div className="col-span-2 min-h-min md:h-screen grid md:grid-cols-2 gap-2">
-              {/* left content */}
-              <div className="h-80 md:h-full relative rounded-2xl overflow-hidden">
-                <div className="absolute inset-0 bg-gray-100 opacity-0 hover:opacity-80 flex justify-center items-center">
-                  <Link
-                    to={""}
-                    class="text-lg font-semibold flex items-center gap-x-2"
-                  >
-                    <span className="text-xl">Click Here</span>
-                    <span>
-                      <FaArrowUpRightFromSquare />
-                    </span>
-                  </Link>
-                </div>
-                <img
-                  loading="lazy"
-                  className="w-full h-full object-cover object-center"
-                  src="https://devoxsoftware.com/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2024/02/philip-oroni-MyY188YX0vU-unsplash.jpg.webp"
-                  alt="AMNDTECH Software"
-                />
-              </div>
-              {/* Right content */}
-              <div className="bg-theme-bg-dark rounded-2xl overflow-hidden p-4 md:p-10">
-                <div className="h-full flex flex-col justify-between">
-                  {/* Top heading  */}
-                  <div className="flex justify-between items-center mb-10">
-                    <div className="flex gap-x-2 items-center">
-                      <span className="rounded p-1 bg-gray-800 text-white">
-                        <IoMdKey />
-                      </span>
-                      <span className="text-gray-600 tracking-wide font-theme-font">
-                        UNDER NDA
-                      </span>
-                    </div>
-                    <div className="text-gray-600 tracking-wide font-theme-font">
-                      SOCIAL MEDIA
-                    </div>
-                  </div>
-                  {/* center content */}
-                  <div className="grow">
-                    {" "}
-                    <h2 className="text-white text-2xl font-semibold text-bold text-start tracking-wide font-theme-font mb-2">
-                      Got a Project in Mind? Let’s Talk!
-                    </h2>
-                    <p className="text-gray-600 text-start tracking-wide font-theme-font mb-2">
-                      Share the details of your project – like scope or business
-                      challenges. Our team will carefully study them and then
-                      we’ll figure out the next move together.
-                    </p>
-                    <form action="" className="flex flex-col gap-y-5">
-                      {/* full name  */}
-                      <div>
-                        <input
-                          placeholder="Your full Name*"
-                          className="w-full text-gray-600 outline-none border-0 font-theme-font bg-theme-bg-dark
-                          border-b border-gray-600 py-2"
-                          type="text"
-                          id="customInput"
-                        />
-                      </div>
-                      {/* phone Number  */}
-                      <div>
-                        <input
-                          placeholder="Your Phone number*"
-                          className="w-full text-gray-600 outline-none border-0 font-theme-font bg-theme-bg-dark
-                          border-b border-gray-600 py-2"
-                          type="text"
-                          id="customInput"
-                        />
-                      </div>
-                      {/* coperate email */}
-                      <div>
-                        <input
-                          placeholder="Corporate email*"
-                          className="w-full text-gray-600 outline-none border-0 font-theme-font bg-theme-bg-dark
-                          border-b border-gray-600 py-2"
-                          type="text"
-                          id="customInput"
-                        />
-                      </div>
-                      {/* about your project  */}
-                      <div>
-                        <input
-                          placeholder="About your Project*"
-                          className="w-full text-gray-600 outline-none border-0 font-theme-font bg-theme-bg-dark
-                          border-b border-gray-600 py-2"
-                          type="text"
-                          id="customInput"
-                        />
-                      </div>
-                    </form>
-                  </div>
-                  {/* button  */}
-                  <div className="flex flex-col md:flex-row md:items-center justify-between gap-x-20 gap-y-5 mt-5">
-                    <div>
-                      <p className="text-white text-xs">
-                        By sending this form I confirm that I have read and
-                        accept the{" "}
-                        <span className="underline hover:no-underline text-white">
-                          Privacy Policy
-                        </span>
-                      </p>
-                    </div>
-                    <div className="flex items-center gap-x-2">
-                      <span className="text-gray-600 text-nowrap font-bold">
-                        View Case Study
-                      </span>
-                      <button className="p-3 text-lg  bg-purple-600 text-white rounded-3xl font-semibold hover:bg-white hover:text-purple-600 hover:shadow-md hover:shadow-purple-600 hover:shoadow-2xl transition-color delay-150">
-                        <HiOutlineArrowLongRight className="text-lg" />
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="relative max-w-6xl px-4 py-20 grid grid-cols-2 gap-x-10 gap-y-10  m-auto">
+            <ContactFrom />
           </div>
         </section>
 
--- client/src/utils/Banner.jsx
+++ client/src/utils/Banner.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { TbMessageChatbot } from "react-icons/tb";
 
 function Banner() {
-  return <div className="fixed bottom-2 right-2 bg-red-400">Banner</div>;
+  return (
+    <div
+      style={{ backgroundColor: "#483285",border:"1px solid #483285" }}
+      className="fixed z-[9999] bottom-4 right-4   shadow-lg rounded-full flex items-center justify-center w-16 h-16 p-4 cursor-pointer hover:scale-110 transition-transform"
+    >
+      <TbMessageChatbot size={32} className="text-white" />
+    </div>
+  );
 }
 
 export default Banner;
